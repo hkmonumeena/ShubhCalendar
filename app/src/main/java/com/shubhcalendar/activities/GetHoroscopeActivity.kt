2a2e3a97782f6e33a4866163ee56ee48c3a722de
monu <monum811@gmail.com>
2021-03-20 17:25:15 +0530

    20 march 2020

---
+++ app/src/main/java/com/shubhcalendar/activities/GetHoroscopeActivity.kt
@@ -0,0 +1,18 @@
+package com.shubhcalendar.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.cardview.widget.CardView
+import com.shubhcalendar.R
+import com.shubhcalendar.utills.Craft.startActivity
+
+class GetHoroscopeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_horoscope)
+        val cardViewArrow = findViewById<CardView>(R.id.cardViewArrow)
+        cardViewArrow.setOnClickListener {
+            startActivity<RashiActivity>()
+        }
+    }
+}
--- app/src/main/java/com/shubhcalendar/activities/HomeActivity.kt
+++ app/src/main/java/com/shubhcalendar/activities/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.shubhcalendar.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.cardview.widget.CardView
 import com.shubhcalendar.R
 import com.shubhcalendar.utills.Craft.putKey
 import com.shubhcalendar.utills.Craft.startActivity
@@ -12,11 +13,13 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val btnClickMe = findViewById<Button>(R.id.btnClickMe)
-        btnClickMe.setOnClickListener {
+
+   val cardViewGetHoroscope = findViewById<CardView>(R.id.cardViewGetHoroscope)
+        cardViewGetHoroscope.setOnClickListener {
             putKey(Keys.userID, "")
             finishAffinity()
-            startActivity<SplashActivity>()
+            startActivity<GetHoroscopeActivity>()
         }
+
     }
 }
