137991085e606d0422b83d3d9a26290d677805aa
abhilashasharma2021 <suhanashuri584@gmail.com>
2021-03-23 09:42:32 +0530

    new

--- app/src/main/java/com/shubhcalendar/activities/HomeActivity.kt
+++ app/src/main/java/com/shubhcalendar/activities/HomeActivity.kt
@@ -3,16 +3,27 @@ package com.shubhcalendar.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.cardview.widget.CardView
+import androidx.core.view.GravityCompat
 import com.shubhcalendar.R
+import com.shubhcalendar.databinding.ActivityHomeBinding
 import com.shubhcalendar.utills.Craft.putKey
 import com.shubhcalendar.utills.Craft.startActivity
 import com.shubhcalendar.utills.Keys
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding=ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rlFestival.setOnClickListener {
+            startActivity<LanguageActivity>()
+        }
+        binding.icMenu.setOnClickListener { binding.drawer.openDrawer(GravityCompat.END) }
+        binding.ivCross.setOnClickListener { binding.drawer.closeDrawer(GravityCompat.END) }
 
    val cardViewGetHoroscope = findViewById<CardView>(R.id.cardViewGetHoroscope)
         cardViewGetHoroscope.setOnClickListener {
