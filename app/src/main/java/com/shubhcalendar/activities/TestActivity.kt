c6c395f94ea85c4af3bc630b6107017581943637
monu <monum811@gmail.com>
2021-03-26 17:47:38 +0530

    26 march 2020

--- app/src/main/java/com/shubhcalendar/activities/TestActivity.kt
+++ app/src/main/java/com/shubhcalendar/activities/TestActivity.kt
@@ -2,76 +2,45 @@ package com.shubhcalendar.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import androidx.cardview.widget.CardView
-import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.michalsvec.singlerowcalendar.calendar.CalendarChangesObserver
+import com.michalsvec.singlerowcalendar.calendar.CalendarViewManager
+import com.michalsvec.singlerowcalendar.calendar.SingleRowCalendarAdapter
+import com.michalsvec.singlerowcalendar.selection.CalendarSelectionManager
+import com.michalsvec.singlerowcalendar.utils.DateUtils.getDates
 import com.shubhcalendar.R
-import com.shubhcalendar.databinding.ActivityHomeBinding
+import com.shubhcalendar.databinding.ActivityTestBinding
+import com.shubhcalendar.utills.Craft
 import com.shubhcalendar.utills.Craft.confirmationDialog
-import com.shubhcalendar.utills.Craft.putKey
-import com.shubhcalendar.utills.Craft.startActivity
 import com.shubhcalendar.utills.GenricAdapter
-import com.shubhcalendar.utills.Keys
 import com.shubhcalendar.utills.ViewHolder
 import kotlinx.android.synthetic.main.calendar_item.view.*
 import java.util.*
+import kotlin.collections.ArrayList
 
-class HomeActivity : AppCompatActivity() {
-    lateinit var binding: ActivityHomeBinding
+class TestActivity : AppCompatActivity() {
     private val calendar = Calendar.getInstance()
     private var currentMonth = 0
     var mutableList: MutableList<ModelMonth> = mutableListOf()
-    val monthNumListShow = arrayListOf<String>("January","February","March","April","May","June","July","August","September","October","November","December")
-    val monthNumList = arrayListOf<String>("0","1","2","3","4","5","6","7","8","9","10","11")
+    val list = arrayListOf<String>()
+    lateinit var APIKEY: String
     var hour: Int = 0
     var minute: Int = 0
     var seconds: Int = 0
-
+    lateinit var binding: ActivityTestBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.rlFestival.setOnClickListener {
-            startActivity<LanguageActivity>()
-        }
-        binding.icMenu.setOnClickListener { binding.drawer.openDrawer(GravityCompat.END) }
-        binding.ivCross.setOnClickListener { binding.drawer.closeDrawer(GravityCompat.END) }
-
-   val cardViewGetHoroscope = findViewById<CardView>(R.id.cardViewGetHoroscope)
-        cardViewGetHoroscope.setOnClickListener {
-            putKey(Keys.userID, "")
-            finishAffinity()
-            startActivity<GetHoroscopeActivity>()
-        }
-
         hour = calendar.get(Calendar.HOUR_OF_DAY)
         minute = calendar.get(Calendar.MINUTE)
         seconds = calendar.get(Calendar.SECOND)
-        binding.textViewDateYear.setOnClickListener {
-            confirmationDialog("Select Month", monthNumListShow,monthNumList)
+        binding.btn.setOnClickListener {
+            confirmationDialog("Name", list, list)
             { position, value, id ->
-                currentMonth=id.toInt()
-                mutableList.clear()
-                getAnyDate(id.toInt()).forEach {
-                    mutableList.add(
-                        ModelMonth(
-                            com.michalsvec.singlerowcalendar.utils.DateUtils.getDayNumber(it),
-                            com.michalsvec.singlerowcalendar.utils.DateUtils.getDay3LettersName(it),
-                            com.michalsvec.singlerowcalendar.utils.DateUtils.getMonthNumber(it),
-                            com.michalsvec.singlerowcalendar.utils.DateUtils.getYear(it),
-                            hour.toString(),
-                            minute.toString()
-                        )
-                    )
-                }
-
-                binding.recyclerMonth.layoutManager =
-                    LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-                binding.recyclerMonth.adapter = AdapterMonth(mutableList as ArrayList<ModelMonth>)
 
             }
         }
@@ -87,16 +56,14 @@ class HomeActivity : AppCompatActivity() {
                     minute.toString()
                 )
             )
-        }
 
+            list.add(com.michalsvec.singlerowcalendar.utils.DateUtils.getMonthNumber(it))
+
+        }
         binding.recyclerMonth.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         binding.recyclerMonth.adapter = AdapterMonth(mutableList as ArrayList<ModelMonth>)
-
-
     }
-
-
 
     private fun getDatesOfNextMonth(): List<Date> {
         currentMonth++ // + because we want next month
@@ -126,16 +93,10 @@ class HomeActivity : AppCompatActivity() {
         return getDates(mutableListOf())
     }
 
-    private fun getAnyDate(selectedMonth:Int): List<Date> {
-        // get all next dates of current month
-        currentMonth--
-        currentMonth =selectedMonth
-        return getDates(mutableListOf())
-    }
-
     private fun getDates(list: MutableList<Date>): List<Date> {
         // load dates of whole month
         Log.e("pihu", "getDates: " + currentMonth)
+
         calendar.set(Calendar.MONTH, currentMonth)
         calendar.set(Calendar.DAY_OF_MONTH, 1)
         list.add(calendar.time)
@@ -154,27 +115,5 @@ class HomeActivity : AppCompatActivity() {
 }
 
 
-class AdapterMonth(items: ArrayList<ModelMonth>) : GenricAdapter<ModelMonth>(items) {
 
 
-    override fun getItemViewType(position: Int): Int {
-        return R.layout.calendar_item
-    }
-
-    override fun configure(item: ModelMonth, holder: ViewHolder, position: Int) {
-        val set = holder.itemView
-        set.textViewDayName.text = item.dayname
-        set.textViewDate.text = item.day
-    }
-
-}
-
-data class ModelMonth(
-    val day: String,
-    val dayname: String,
-    val monthNum: String,
-    val year: String,
-    val hour: String,
-    val minute: String,
-
-    )
